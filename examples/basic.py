267c38b2769a2bbb7f3fe7116958a8a2cb06d0a9
Dean Maniatis <deanmaniatis@gmail.com>
2017-12-15 17:17:38 +0200

    Another old ref. bites the dust

--- examples/basic.py
+++ examples/basic.py
@@ -26,7 +26,7 @@ def connect_handler(data):
     channel = pusher.subscribe("test_channel")
 
     channel.bind('my_event', channel_callback)
-    
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     appkey = sys.argv[1]
 
-    pusher = pusherclient.Pusher(appkey)
+    pusher = pysher.Pusher(appkey)
 
     pusher.connection.bind('pusher:connection_established', connect_handler)
     pusher.connect()
